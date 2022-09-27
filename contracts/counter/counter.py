033ca61770922652e91c482b0ca246731f2a1128
elia-furlanis <elia.furl@gmail.com>
2022-09-28 00:18:51 +0200

    Added export of TEAL and ABI to counter.py

--- contracts/counter/counter.py
+++ contracts/counter/counter.py
@@ -32,3 +32,24 @@ class CounterApp(Application):
             self.counter.set(self.counter - Int(1)),
             output.set(self.counter),
         )
+
+if __name__ == "__main__":
+
+    approval_filename = "./build/approval.teal"
+    clear_filename = "./build/clear.teal"
+    interface_filename = "./build/contract.json"
+    
+    app = CounterApp()
+
+    # export TEAL and ABI in build folder
+    with open(approval_filename, "w") as f:
+        f.write(app.approval_program)
+
+    with open(clear_filename, "w") as f:
+        f.write(app.clear_program)
+
+    import json
+    with open(interface_filename, "w") as f:
+        f.write(json.dumps(app.contract.dictify()))
+    
+    print('\n------------TEAL generation completed!------------\n')
